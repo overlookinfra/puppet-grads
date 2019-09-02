fb60a6c169a68d1a090c045ad601a415d9db766a
Andrew Hayes <andrew.hayes@puppetlabs.com>
2019-09-02 15:24:01 +0100

    Updates working and layout slightly for 2019

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import {
 class App extends Component {
   imagePath = process.env.PUBLIC_URL + '/assets/'
   mobileMaxWidth = "459"
-  desktopMinWidth = "460"
+  desktopMinWidth = "600"
 
   responsive_image(name) {
     return (
@@ -35,13 +35,13 @@ class App extends Component {
 
   render_interested() {
     let title = "We think you'll like what you see."
-    let desc = "If you like challenges and want to apply to one of the leading automation companies in the world then we'd like to hear from you! Click below to apply."
+    let desc = "We offer a robust program that places interns directly on teams to collaborate on projects, while also providing on-the-job training to equip you with the tools and knowledge to be successful. Our goal is to convert most of our interns into full-time, graduate-level positions at the end of the internship, so each intern is assigned a manager and mentor to ensure that you have the support and guidance needed!! Click below to apply."
     let button = (<Button color="yellow" as="a" href={"https://puppet.com/company/careers/jobs?gh_jid=1405357"}>
               Apply for Placement
             </Button>)
 
     if (((new Date()).getTime() / 1000) <  1572307200) {
-        desc = "If you like challenges and want to apply to one of the leading automation companies in the world then we'd like to hear from you! Let us know you're interested by clicking below"
+        desc = "We offer a robust program that places interns directly on teams to collaborate on projects, while also providing on-the-job training to equip you with the tools and knowledge to be successful. Our goal is to convert most of our interns into full-time, graduate-level positions at the end of the internship, so each intern is assigned a manager and mentor to ensure that you have the support and guidance needed! Let us know you're interested by clicking below"
         button = (<Button color="yellow" as="a" href={"https://puppetbelfast.typeform.com/to/Lh3oWC"}>
             I'm interested!
         </Button>)
@@ -78,9 +78,9 @@ class App extends Component {
 
   render_register() {
     const title = "Puppet Open Doors"
-    const date = "Date: 25th October 2018 @ 6pm"
-    const desc = "We are holding a Meetup for both Graduates and Interns to get a feel for Puppet and to experience how we work here. Eat pizza, drink beer (including non-alcoholic drinks), and more importantly, have fun!"
-    const desc2 = "There are plenty of fellow engineers around for a chat, and we do not believe in stupid questions here :) If you are currently studying Computer Science or Software Engineering and are interested in the Graduate/Intern positions, join us for a night of exciting activities! If we don't already have your details, Click \"I'm Interested!\" and let us know if you are attending!"
+    const date = "Date: 29th October 2019 @ 6pm"
+    const desc = "We are holding a Meetup for Interns to get a feel for Puppet and to experience how we work here. Eat pizza, drink beer (including non-alcoholic drinks), and more importantly, have fun!"
+    const desc2 = "There are plenty of fellow engineers around for a chat, and we do not believe in stupid questions here :) If you are currently studying Computer Science or Software Engineering and are interested in the Intern position, join us for a night of exciting activities! If we don't already have your details, Click \"I'm Interested!\" and let us know if you are attending!"
     return (
       <div className={"ui-section"}>
         <Divider />
@@ -104,31 +104,25 @@ class App extends Component {
   }
 
   mobile_pups() {
+    const availableDogImages = ['miku_needs_you.jpg', 'rosie.jpg', 'poppy.jpg', 'betty.jpg', 'kula.jpg', 'darwin.jpg']
+    let selectedDogImages = []
+    for(var i = availableDogImages.length-1;i>=4;i--){
+        const dog = availableDogImages.splice(Math.floor(Math.random()*availableDogImages.length), 1)
+        selectedDogImages.push(dog)
+    }
+
+    const dogOutput = selectedDogImages.map((dog) => {
+        return (
+            <Grid.Column>
+            <Image src={`${this.imagePath}${dog}`} />
+          </Grid.Column>
+        )
+    })
+
     return (
       <Grid columns={2} style={{ textAlign: "center", margin: "auto" }}>
         <Grid.Row>
-          <Grid.Column style={{ textAlign: "center", margin: "auto" }}>
-            <Image src={`${this.imagePath}miku_needs_you.jpg`} size="small" />
-          </Grid.Column>
-          <Grid.Column style={{ textAlign: "center", margin: "auto" }}>
-            <Image src={`${this.imagePath}rosie.jpg`} size="small" />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column style={{ textAlign: "center", margin: "auto" }}>
-            <Image src={`${this.imagePath}poppy.jpg`} size="small" />
-          </Grid.Column>
-          <Grid.Column style={{ textAlign: "center", margin: "auto" }}>
-            <Image src={`${this.imagePath}betty.jpg`} size="small" />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column style={{ textAlign: "center", margin: "auto" }}>
-            <Image src={`${this.imagePath}kula.jpg`} size="small" />
-          </Grid.Column>
-          <Grid.Column style={{ textAlign: "center", margin: "auto" }}>
-            <Image src={`${this.imagePath}darwin.jpg`} size="small" />
-          </Grid.Column>
+          {dogOutput}
         </Grid.Row>
       </Grid>
 
@@ -136,29 +130,25 @@ class App extends Component {
   }
 
   desktop_pups() {
+    const availableDogImages = ['miku_needs_you.jpg', 'rosie.jpg', 'poppy.jpg', 'betty.jpg', 'kula.jpg', 'darwin.jpg']
+    let selectedDogImages = []
+    for(var i = availableDogImages.length-1;i>=3;i--){
+        const dog = availableDogImages.splice(Math.floor(Math.random()*availableDogImages.length), 1)
+        selectedDogImages.push(dog)
+    }
+
+    const dogOutput = selectedDogImages.map((dog) => {
+        return (
+            <Grid.Column>
+            <Image src={`${this.imagePath}${dog}`} />
+          </Grid.Column>
+        )
+    })
+
     return (
       <Grid columns={3} divided style={{ textAlign: "center" }}>
         <Grid.Row>
-          <Grid.Column>
-            <Image src={`${this.imagePath}miku_needs_you.jpg`} />
-          </Grid.Column>
-          <Grid.Column>
-            <Image src={`${this.imagePath}rosie.jpg`} />
-          </Grid.Column>
-          <Grid.Column>
-            <Image src={`${this.imagePath}poppy.jpg`} />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <Image src={`${this.imagePath}betty.jpg`} />
-          </Grid.Column>
-          <Grid.Column>
-            <Image src={`${this.imagePath}kula.jpg`} />
-          </Grid.Column>
-          <Grid.Column>
-            <Image src={`${this.imagePath}darwin.jpg`} />
-          </Grid.Column>
+          {dogOutput}
         </Grid.Row>
       </Grid>
     )
@@ -175,9 +165,10 @@ class App extends Component {
     )
   }
 
+  // choose 3 dogs at random
   render_meet_team() {
     const title = "Meet the Team"
-    const desc = "Puppet is a dog friendly office. Miku is in most days and the others come in too from time to time."
+    const desc = "Puppet is a dog friendly office. These are some of the pups that might come in from time to time."
     return (
       <div style={{ "marginTop": "2em" }} className={"ui-section"}>
         <Divider />
@@ -194,8 +185,8 @@ class App extends Component {
     )
   }
 
-  render_open_source() {
-    const desc = "We are also an Open Source company and working with us is an excellent way to get your name out in the Open Source community."
+  render_learning() {
+    const desc = "As an intern you won't be expected to know most of the technologies above. The main thing we will expect of you as an intern is to learn. You will be paired with a mentor to help you get farmiliar with these technologies. You will also have your team to support you and help you reach your potential as an engineer."
     return (
       <p style={{ "lineHeight": "1.7" }} className={"info-text ui-section"}>
         {desc}
@@ -204,7 +195,7 @@ class App extends Component {
   }
 
   render_peer() {
-    const desc = "At Puppet you aren't just put in the corner and expected to make Tea for the senior engineers. As a Grad/Intern you'll be placed in a team with other engineers and treated as a peer."
+    const desc = "At Puppet you aren't just put in the corner and expected to make Tea for the senior engineers. As a Intern you'll be placed in a team with other engineers and treated as a peer."
     const desc2 = "You'll work on real projects - it's hard work, but if you fancy the challenge we'd like to hear from you!"
     return (
       <p style={{ "marginBottom": "1em", "lineHeight": "1.7" }} className={"info-text ui-section"}>
@@ -217,6 +208,10 @@ class App extends Component {
   }
 
 
+  // link to kezzie blog
+  // add summary about inernships from puppet website / replace openner
+  // mention agile and team structures. 
+
   render() {
     return (
       <div className="App">
@@ -228,6 +223,7 @@ class App extends Component {
             <Grid.Row columns={1}>
               <Grid.Column>
                 {this.render_interested()}
+                {this.render_register()}
                 {this.render_meet_team()}
                 <Divider />
                 <Message info style={{ "marginTop": "1em" }} className={"info-text"}>
@@ -246,9 +242,12 @@ class App extends Component {
                       <List.Item>Automated testing (Ruby, Selenium, Nightwatch)</List.Item>
                       <List.Item>DevOps and Configuration Management </List.Item>
                     </List>
+                    <p style={{ "lineHeight": "1.7" }}>
+                      You will also learn about agile methodologies and gain experience working in an agile environment. 
+                    </p>
                   </Message.Content>
                 </Message>
-                {this.render_open_source()}
+                {this.render_learning()}
               </Grid.Column>
             </Grid.Row>
           </Grid>
@@ -275,6 +274,7 @@ class App extends Component {
                       <List.Item>Free beer, cider, wine etc..</List.Item>
                       <List.Item>Lego, games consoles, board games, table tennis</List.Item>
                       <List.Item>A state-of-the-art coffee machine</List.Item>
+                      <List.Item>Regular work events, such as trips to "Let's go Hydro!" and BBQs</List.Item>
                     </List>
                   </Message.Content>
                 </Message>
@@ -305,6 +305,7 @@ class App extends Component {
                       <List.Item>GDG</List.Item>
                       <List.Item>Django Girls</List.Item>
                       <List.Item>Women Tech Space</List.Item>
+                      <List.Item>OWASP Belfast</List.Item>
                     </List>
                     <p style={{ "lineHeight": "1.7" }}>
                         We also get a number of days a year free to volunteer for charities. Over last few years we have helped the likes of:
@@ -321,7 +322,12 @@ class App extends Component {
               </Grid.Column>
             </Grid.Row>
           </Grid>
+          <br/>
+            <p className={"info-text ui-section"}>
+                If you'd like to read more about working at puppet, one of our previous interns, Kezie, has written about it on our <a href="https://puppet.com/blog/lessons-year-puppet">blog</a>.
+            </p>
         </div>
+        
         <br/>
         <br/>
         {this.responsive_image('divider3')}
